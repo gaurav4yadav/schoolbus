b874ac3cd0c0675d7b0fbbfe7adc43e665739228
gaurav4yadav <gauravyadav.viid@gmail.com>
2021-07-18 17:14:04 +0530

    Initial commit

---
+++ app/src/main/java/com/slef/schoolbustracker/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.slef.schoolbustracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun studentloginfunc(view: View) {
+
+        val intent = Intent(this,studentLogin::class.java)
+        startActivity(intent)
+    }
+    fun instituteloginfunc(view: View) {
+        val intent =Intent (this,institueLogin::class.java)
+        startActivity(intent)
+    }
+
+    fun registerfun(view: View) {
+
+        val intent =Intent (this,registerpage::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/slef/schoolbustracker/institueLogin.kt
@@ -0,0 +1,12 @@
+package com.slef.schoolbustracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class institueLogin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_institue_login)
+    }
+}
