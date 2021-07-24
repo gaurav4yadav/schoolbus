19f38605ad2eabdbb897bc3f26b4892f7335c671
gaurav4yadav <gauravyadav.viid@gmail.com>
2021-07-24 23:14:25 +0530

    new once with enhanced location

---
+++ app/src/main/java/com/slef/schoolbustracker/dc.kt
@@ -0,0 +1,16 @@
+package com.slef.schoolbustracker
+
+class dc {
+
+
+    var gmail = ""
+    var latitute=0.0
+    var longitude= 0.0
+
+    constructor(email:String ,lati:Double,longi:Double)
+    {
+        gmail=email
+        latitute= lati
+        longitude=longi
+    }
+}
--- app/src/main/java/com/slef/schoolbustracker/models/myuser.kt
+++ app/src/main/java/com/slef/schoolbustracker/models/myuser.kt
@@ -1,18 +1,31 @@
 package com.slef.schoolbustracker.models
 
+import com.google.firebase.auth.FirebaseAuth
+
+private lateinit var auth: FirebaseAuth
+
+val user = auth.currentUser
 class myuser {
 
-    var gmail = ""
+
+
+    var qmail = ""
+    var gid=""
+    var name=""
     var passwordid = ""
     var uniqueid =""
  public   var phoneid =""
 
-    constructor(email:String ,  password:String , uniqueid:String, phoneid:String)
+    constructor(email:String ,  password:String , uniqueid:String, phoneid:String ,gid:String,name:String)
     {
-        gmail=email
+        qmail=email
         passwordid=password
         this.uniqueid=uniqueid
         this.phoneid=phoneid
+        //this.gid =user?.uid.toString()
     }
+constructor()
+{
 
+}
 }
